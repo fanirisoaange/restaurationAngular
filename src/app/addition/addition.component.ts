906d51bea03c945335bbcc9b8442e9f808d3138a
fanirisoa <fanirisoaange@gmail.com>
2020-05-19 22:46:28 +0300

    initial commit

---
+++ src/app/addition/addition.component.ts
@@ -0,0 +1,122 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addition',
+  templateUrl: './addition.component.html',
+  styleUrls: ['./addition.component.css']
+})
+export class AdditionComponent implements OnInit {
+
+  recipes: any;
+  recipe: any;
+  drinks: any;
+  recipeTotalPrice = 0;
+  drinkTotalPrice = 0;
+  totalPrice = 0;
+  DISCOUNT = 20;
+  discountMessage = '';
+
+  constructor() { }
+
+  ngOnInit() {
+    this.initRecipe();
+    this.initDrink();
+  }
+
+  initRecipe() {
+    this.recipes = [
+      {
+        key: 0,
+        value: 'Soupe',
+        ingredients: [
+          { value: 'Laitue', price: 0 },
+          { value: 'tomates', price: 0 },
+          { value: 'fromages', price: 0 },
+          { value: 'pain complet', price: 0 }
+        ]
+      },
+      {
+        key: 1,
+        value: 'Pizza',
+        ingredients: [
+          { value: 'Jarret', price: 0 },
+          { value: 'pommes de terres', price: 0 },
+          { value: 'carottes', price: 0 },
+          { value: 'champignons', price: 0 },
+          { value: 'Courgettes', price: 0 },
+          { value: 'Curry', price: 0 }
+        ]
+      },
+      {
+        key: 2,
+        value: 'Salade',
+        ingredients: [
+          { value: 'fromages', price: 0 },
+          { value: 'olives', price: 0 },
+          { value: 'crevettes', price: 0 },
+          { value: 'thon', price: 0 },
+          { value: 'tomates', price: 0 }
+        ]
+      }
+    ];
+    this.recipe = this.recipes[0];
+  }
+
+  initDrink() {
+    this.drinks = [
+      { value: '', price: 0 }
+    ];
+  }
+
+  addIngredient(): void {
+    this.recipes = this.recipes.map(recipe => {
+      if (recipe.key === this.recipe.key) {
+        recipe.ingredients.push({ value: '', price: 0 });
+      }
+      return recipe;
+    });
+  }
+
+  deleteIngredient(item): void {
+    this.recipes = this.recipes.map(recipe => {
+      if (recipe.key === this.recipe.key) {
+        recipe.ingredients.splice(item, 1);
+      }
+      return recipe;
+    });
+    this.setIngredientTotalPrice();
+  }
+
+  setIngredientTotalPrice() {
+    this.recipeTotalPrice = this.recipes.find(recipe => recipe.key === this.recipe.key).ingredients
+    .reduce((accumulateur, current) => accumulateur + parseInt(current.price, 10), 0);
+    this.setTotalPrice();
+  }
+
+  onChangeRecipe(event) {
+    this.recipe = this.recipes.find(recipe => recipe.key === +event.value);
+    this.setIngredientTotalPrice();
+  }
+
+  addDrink(): void {
+    this.drinks.push({ value: '', price: 0 });
+  }
+
+  deleteDrink(item): void {
+    this.drinks.splice(item, 1);
+    this.setDrinkTotalPrice();
+  }
+
+  setDrinkTotalPrice(): void {
+    this.drinkTotalPrice = this.drinks
+    .reduce((accumulateur, current) => accumulateur + parseInt(current.price, 10), 0);
+    this.setTotalPrice();
+  }
+
+  setTotalPrice() {
+    const total = this.drinkTotalPrice + this.recipeTotalPrice ;
+    this.totalPrice = this.drinkTotalPrice <= 100000 ? total : (total - (total * this.DISCOUNT / 100 ));
+    this.discountMessage = this.drinkTotalPrice <= 100000 ? `` : `(Remise de ${this.DISCOUNT}%)`;
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AdditionComponent } from './addition/addition.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import localeFr from '@angular/common/locales/fr';
+import localeFrExtra from '@angular/common/locales/extra/fr';
+import { registerLocaleData } from '@angular/common';
+
+// Angular Material
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+
+registerLocaleData(localeFr, 'fr', localeFrExtra);
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdditionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ],
 })
 export class AppModule { }
